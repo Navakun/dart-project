95ee8255ea0233674f92e9f90dc5f8dd2ca71288
Navakun <kunkid28147@gmail.com.com>
2024-11-18 15:33:56 +0700

    first commit

---
+++ hello.dart
@@ -0,0 +1,12 @@
+void main(){
+  int num1 = 10;
+  int num2 = 3;
+
+  int div = num1~/num2;
+  int mod = num1%num2;
+  
+
+  print("output");
+  print("quotient = $div");
+  print("remainder = $mod");
+}
