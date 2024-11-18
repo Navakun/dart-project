d5492df640db97f74a8cf0a9c8f5a4ca41730f94
Navakun <kunkid28147@gmail.com.com>
2024-11-18 16:51:51 +0700

    new file

---
+++ share.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(){
+
+  print('total bill :');
+  int? bill = int.parse(stdin.readLineSync()!);
+  print('number of people :');
+  int? people = int.parse(stdin.readLineSync()!);
+  
+  double formula = bill/people;
+
+  print('each person need to pay is $formula'); 
+}
